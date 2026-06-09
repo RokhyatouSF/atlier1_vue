b51b0ebfc0459cea7e430c4a1398478de652aabf
RokhyatouSF <rokhyatousarrfall@gmail.com>
2026-06-09 15:24:08 +0000

    créez un nouveau composant CardIdee

---
+++ atelier1/src/components/CardIdee.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <h2>Titre</h2>
+    <p>Description</p>
+    <button>Voter</button>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  padding: 15px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+}
+
+button {
+  background: blue;
+  color: white;
+  padding: 5px 10px;
+}
+</style>
